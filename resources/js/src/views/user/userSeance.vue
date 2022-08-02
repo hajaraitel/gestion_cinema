ff39bde19e9c89b37b6c1fef5b37a7e2606a833c
hajarait <hajaraitel@gmail.com>
2022-08-02 18:36:34 +0100

    user seances

--- resources/js/src/views/user/userSeance.vue
+++ resources/js/src/views/user/userSeance.vue
@@ -1,10 +1,53 @@
-<template>
 
+<template>
+  <v-card>
+    <v-card-title>
+      Seances
+      <v-spacer></v-spacer>
+      <v-text-field
+        v-model="search"
+        append-icon="mdi-magnify"
+        label="Search"
+        single-line
+        hide-details
+      ></v-text-field>
+    </v-card-title>
+    <v-data-table
+      :headers="headers"
+      :items="desserts"
+      :search="search"
+    ></v-data-table>
+  </v-card>
 </template>
 <script>
-export default{
-mounted(){
-    console.log(this.$route.params.idFilm)
-}
-}
+import axiosClient from '@/axios'
+  export default {
+  methods: {
+    getSeances(){
+        axiosClient.get('/seances').then(resp => {this.seances=resp.data});
+    }
+
+  },
+    data () {
+      return {
+        search: '',
+        headers: [
+          {
+            text: 'Film',
+            align: 'start',
+            sortable: true,
+            value: 'name',
+          },
+          { text: 'Salle', value: 'calories' },
+          { text: 'Date seance', value: 'fat' },
+          { text: 'Horaire', value: 'carbs' },
+          { text: 'Prix', value: 'protein' },
+          { text: 'Version', value: 'iron' },
+          { text: 'Capacité', value: 'iron' },
+          { text: 'Reservation', value: 'iron' },
+        ],
+        seances: [],
+      }
+    },
+  }
 </script>
